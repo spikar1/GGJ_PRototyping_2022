93e35f96bef31d6ca448b9d429de466667bd6dea
Kryxzael <kasper.ramskjell98@gmail.com>
2022-01-29 20:34:24 +0100

    Add some delay to the play-mode switch trigger thingys

--- Assets/Scripts/PlayModeSwitchTrigger.cs
+++ Assets/Scripts/PlayModeSwitchTrigger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,32 @@ using UnityEngine;
 
 public class PlayModeSwitchTrigger : MonoBehaviour
 {
+    private bool _hasBeenTriggered;
+
+    private const float ENTER_DELAY_TIME = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_hasBeenTriggered)
+            return;
+
         if (collision.gameObject.IsPlayer())
         {
-            //TODO: Field should probably (maybe?) disappear after the player exits puzzle mode
-            PlayModeManager.Instance.CurrentMode = PlayModeManager.PlayMode.Puzzle;
-            Destroy(gameObject);
+            _hasBeenTriggered = true;
+            StartCoroutine(nameof(CoEnterSwitch));
         }
+    }
+
+    IEnumerator CoEnterSwitch()
+    {
+        //Sorry...
+        Time.timeScale = 0f;
+        //End sorry
+
+        yield return new WaitForSecondsRealtime(ENTER_DELAY_TIME);
+        PlayModeManager.Instance.CurrentMode = PlayModeManager.PlayMode.Puzzle;
+
+        yield return new WaitUntil(() => PlayModeManager.Instance.CurrentMode != PlayModeManager.PlayMode.Puzzle);
+        Destroy(gameObject);
     }
 }
